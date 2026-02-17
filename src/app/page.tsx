f44966564a68295b2f4bfc1bb855155071381623
Sanchit Tikhile <157561024+Itachi07l@users.noreply.github.com>
2026-02-17 07:43:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -238,7 +238,6 @@ export default function Home() {
   pillColor="#ffffff"
   hoveredPillTextColor="#ffffff"
   pillTextColor="#000000"
-  theme="light"
   initialLoadAnimation={false}
 />
 			<Nav />
